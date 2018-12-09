130f8316571f5543f74f54b19c543448ecbdcf9c
Felix <felix@canvasheroes.com>
2018-12-09 22:47:02 +0100

    Add basic nodejs backend

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+const server = express()
+const PORT = process.env.PORT || 3020
+
+server.use(express.static(path.join(__dirname, 'build')))
+
+server.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+server.listen(PORT, () => { console.log(`Website runs at http://localhost:${PORT}/`) })
