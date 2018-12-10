8ebdf715f773f505376ae2a6a54ae1f9fc7a4ac0
Felix <felix@canvasheroes.com>
2018-12-10 15:31:45 +0100

    get times in footer also from the api

--- src/components/_shared/footer.jsx
+++ src/components/_shared/footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const Footer = () => (
+const Footer = ({ times }) => (
     <footer>
         <div className="info__container">
             <div className="inner">
@@ -13,26 +14,12 @@ const Footer = () => (
                 <div className="opening-times">
                     <h4>Öffnungszeiten</h4>
                     <div className="week">
-                        <div className="item">
-                            <div className="day">Montag</div>
-                            <div className="time">8:00 – 14:00 Uhr und	17:00 – 22:00 Uhr</div>
-                        </div>
-                        <div className="item">
-                            <div className="day">Dienstag</div>
-                            <div className="time">8:00 – 14:00 Uhr	und	15:00 – 19:00 Uhr</div>
-                        </div>
-                        <div className="item">
-                            <div className="day">Mittwoch</div>
-                            <div className="time">8:00 – 14:00 Uhr</div>
-                        </div>
-                        <div className="item">
-                            <div className="day">Donnerstag</div>
-                            <div className="time">8:00 – 14:00 Uhr</div>
-                        </div>
-                        <div className="item">
-                            <div className="day">Freitag</div>
-                            <div className="time">8:00 – 13:30 Uhr	und	14:00 – 19:00 Uhr</div>
-                        </div>
+                        {times.map((time, i) => (
+                            <div key={i} className="item">
+                                <div className="day">{time.day}</div>
+                                <div className="time">{time.value}</div>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
@@ -48,5 +35,8 @@ const Footer = () => (
         </div>
     </footer>
 )
+const mapStateToProps = state => ({
+    times: state.global.times
+})
 
-export default Footer
+export default connect(mapStateToProps, null)(Footer)
